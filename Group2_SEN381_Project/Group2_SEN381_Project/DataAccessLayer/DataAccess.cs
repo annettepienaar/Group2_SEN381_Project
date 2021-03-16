962aeaf46b9c38da340c9d705981757df3ae63b5
Annette Pienaar <576370@student.belgiumcampus.ac.za>
2021-03-16 18:40:25 +0200

    Added DataAccess class

---
+++ Group2_SEN381_Project/Group2_SEN381_Project/DataAccessLayer/DataAccess.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group2_SEN381_Project.DataAccessLayer
+{
+    class DataAccess
+    {
+        private string connString;
+
+        public DataAccess()
+        {
+            //Constructor
+        }
+
+        /*public List<Client> getClient(string client_ID) {
+            Method that will get the client and return it as a List
+        }*/
+
+        /*public List<Employee> getEmployee(string emp_ID)
+        {
+            Method that will get the employee and return it as a List
+        }*/
+
+        /*public List<ServicePackage> getSP(string ) 
+        {
+            Method that will get the service package and return it as a List
+        }*/
+
+        /*public Call getCallLogs(DateTime, DateTime)
+        {
+            Method that will get the call log based on the start and end time/date
+        }*/
+
+        /*public void addClient(Client)
+        {
+            Method that adds a Client to the database and Client table
+        }*/
+
+        /*public void addEmployee(Employee)
+        {
+            Method that adds a Employee to the database and Employee table
+        }*/
+
+        /*public void addSP(ServicePackage)
+        {
+            Method that adds a Service Package to the database and ServicePackage table
+        }*/
+
+        /*public void addCallLog(Call)
+        {
+            Method that adds a Call Log to the database and Call table
+        }*/
+
+        /*public void modifyClient(Client)
+        {
+            Method that can update and modify a record in the Client table
+        }*/
+
+        /*public void modifyEmployee(Employee)
+        {
+            Method that can update and modify a record in the Employee table
+        }*/
+
+        /*public void modifySP(ServicePackage)
+        {
+            Method that can update and modify a record in the ServicePackage table
+        }*/
+    }
+}
