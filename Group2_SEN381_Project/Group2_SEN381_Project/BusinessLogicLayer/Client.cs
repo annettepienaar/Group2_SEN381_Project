7838537a09daf73bd25c7c2fcf0177123cebbcac
Dian Potgieter <jyisawesome1234@gmail.com>
2021-03-25 09:44:29 +0200

    moved into folder

---
+++ Group2_SEN381_Project/Group2_SEN381_Project/BusinessLogicLayer/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group2_SEN381_Project
+{
+    abstract class Client
+    {
+        private string clientID, clientName, clientPhoneNum, clientAddress;
+        private List<ServicePackage> servicePackage;
+
+        public abstract void getPhoneNumber();
+        public abstract void getAddress();
+        public abstract void getServicePackages();
+
+
+    }
+}
