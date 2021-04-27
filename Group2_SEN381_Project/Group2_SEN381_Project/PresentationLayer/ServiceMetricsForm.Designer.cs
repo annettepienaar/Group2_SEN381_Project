229719b0cbc5f636770777768d1189cde30a56d4
Jacques-BGCampus <576464@student.belgiumcampus.ac.za>
2021-04-27 22:20:40 +0200

    Added service metrics form design

--- Group2_SEN381_Project/Group2_SEN381_Project/PresentationLayer/ServiceMetricsForm.Designer.cs
+++ Group2_SEN381_Project/Group2_SEN381_Project/PresentationLayer/ServiceMetricsForm.Designer.cs
@@ -28,12 +28,235 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "ServiceMetricsForm";
+			this.lblPerformanceAmount = new System.Windows.Forms.Label();
+			this.lblperformanceTitle = new System.Windows.Forms.Label();
+			this.lblSubcriberAmount = new System.Windows.Forms.Label();
+			this.lblSubcriberAmountTitle = new System.Windows.Forms.Label();
+			this.txtDescription = new System.Windows.Forms.TextBox();
+			this.btnUpdatePackage = new System.Windows.Forms.Button();
+			this.txtCloseDate = new System.Windows.Forms.TextBox();
+			this.lblCloseDateTitle = new System.Windows.Forms.Label();
+			this.txtReleaseDate = new System.Windows.Forms.TextBox();
+			this.lblDescripionTitle = new System.Windows.Forms.Label();
+			this.lblReleaseDateTitle = new System.Windows.Forms.Label();
+			this.lblPackageName = new System.Windows.Forms.Label();
+			this.lblPackageNameTitle = new System.Windows.Forms.Label();
+			this.pnlTicketDisplay = new System.Windows.Forms.Panel();
+			this.btnAddServicePackage = new System.Windows.Forms.Button();
+			this.lbServicePackages = new System.Windows.Forms.ListBox();
+			this.lblServicePackagesTitle = new System.Windows.Forms.Label();
+			this.pnlTicketDisplay.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// lblPerformanceAmount
+			// 
+			this.lblPerformanceAmount.AutoSize = true;
+			this.lblPerformanceAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblPerformanceAmount.Location = new System.Drawing.Point(415, 396);
+			this.lblPerformanceAmount.Name = "lblPerformanceAmount";
+			this.lblPerformanceAmount.Size = new System.Drawing.Size(133, 20);
+			this.lblPerformanceAmount.TabIndex = 42;
+			this.lblPerformanceAmount.Text = "PERFORMANCE";
+			// 
+			// lblperformanceTitle
+			// 
+			this.lblperformanceTitle.AutoSize = true;
+			this.lblperformanceTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblperformanceTitle.Location = new System.Drawing.Point(218, 396);
+			this.lblperformanceTitle.Name = "lblperformanceTitle";
+			this.lblperformanceTitle.Size = new System.Drawing.Size(104, 20);
+			this.lblperformanceTitle.TabIndex = 41;
+			this.lblperformanceTitle.Text = "Performance:";
+			// 
+			// lblSubcriberAmount
+			// 
+			this.lblSubcriberAmount.AutoSize = true;
+			this.lblSubcriberAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblSubcriberAmount.Location = new System.Drawing.Point(415, 354);
+			this.lblSubcriberAmount.Name = "lblSubcriberAmount";
+			this.lblSubcriberAmount.Size = new System.Drawing.Size(77, 20);
+			this.lblSubcriberAmount.TabIndex = 40;
+			this.lblSubcriberAmount.Text = "AMOUNT";
+			// 
+			// lblSubcriberAmountTitle
+			// 
+			this.lblSubcriberAmountTitle.AutoSize = true;
+			this.lblSubcriberAmountTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblSubcriberAmountTitle.Location = new System.Drawing.Point(216, 354);
+			this.lblSubcriberAmountTitle.Name = "lblSubcriberAmountTitle";
+			this.lblSubcriberAmountTitle.Size = new System.Drawing.Size(167, 20);
+			this.lblSubcriberAmountTitle.TabIndex = 39;
+			this.lblSubcriberAmountTitle.Text = "Amount of Subcribers:";
+			// 
+			// txtDescription
+			// 
+			this.txtDescription.Location = new System.Drawing.Point(215, 198);
+			this.txtDescription.Multiline = true;
+			this.txtDescription.Name = "txtDescription";
+			this.txtDescription.Size = new System.Drawing.Size(578, 130);
+			this.txtDescription.TabIndex = 38;
+			// 
+			// btnUpdatePackage
+			// 
+			this.btnUpdatePackage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnUpdatePackage.Location = new System.Drawing.Point(216, 124);
+			this.btnUpdatePackage.Name = "btnUpdatePackage";
+			this.btnUpdatePackage.Size = new System.Drawing.Size(101, 31);
+			this.btnUpdatePackage.TabIndex = 37;
+			this.btnUpdatePackage.Text = "Update";
+			this.btnUpdatePackage.UseVisualStyleBackColor = true;
+			// 
+			// txtCloseDate
+			// 
+			this.txtCloseDate.Location = new System.Drawing.Point(352, 89);
+			this.txtCloseDate.Name = "txtCloseDate";
+			this.txtCloseDate.Size = new System.Drawing.Size(100, 20);
+			this.txtCloseDate.TabIndex = 36;
+			// 
+			// lblCloseDateTitle
+			// 
+			this.lblCloseDateTitle.AutoSize = true;
+			this.lblCloseDateTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblCloseDateTitle.Location = new System.Drawing.Point(212, 89);
+			this.lblCloseDateTitle.Name = "lblCloseDateTitle";
+			this.lblCloseDateTitle.Size = new System.Drawing.Size(92, 20);
+			this.lblCloseDateTitle.TabIndex = 35;
+			this.lblCloseDateTitle.Text = "Close Date:";
+			// 
+			// txtReleaseDate
+			// 
+			this.txtReleaseDate.Location = new System.Drawing.Point(352, 54);
+			this.txtReleaseDate.Name = "txtReleaseDate";
+			this.txtReleaseDate.Size = new System.Drawing.Size(100, 20);
+			this.txtReleaseDate.TabIndex = 34;
+			// 
+			// lblDescripionTitle
+			// 
+			this.lblDescripionTitle.AutoSize = true;
+			this.lblDescripionTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblDescripionTitle.Location = new System.Drawing.Point(212, 175);
+			this.lblDescripionTitle.Name = "lblDescripionTitle";
+			this.lblDescripionTitle.Size = new System.Drawing.Size(93, 20);
+			this.lblDescripionTitle.TabIndex = 33;
+			this.lblDescripionTitle.Text = "Description:";
+			// 
+			// lblReleaseDateTitle
+			// 
+			this.lblReleaseDateTitle.AutoSize = true;
+			this.lblReleaseDateTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblReleaseDateTitle.Location = new System.Drawing.Point(211, 54);
+			this.lblReleaseDateTitle.Name = "lblReleaseDateTitle";
+			this.lblReleaseDateTitle.Size = new System.Drawing.Size(111, 20);
+			this.lblReleaseDateTitle.TabIndex = 32;
+			this.lblReleaseDateTitle.Text = "Release Date:";
+			// 
+			// lblPackageName
+			// 
+			this.lblPackageName.AutoSize = true;
+			this.lblPackageName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblPackageName.Location = new System.Drawing.Point(348, 19);
+			this.lblPackageName.Name = "lblPackageName";
+			this.lblPackageName.Size = new System.Drawing.Size(86, 20);
+			this.lblPackageName.TabIndex = 31;
+			this.lblPackageName.Text = "PACKAGE";
+			// 
+			// lblPackageNameTitle
+			// 
+			this.lblPackageNameTitle.AutoSize = true;
+			this.lblPackageNameTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblPackageNameTitle.Location = new System.Drawing.Point(211, 19);
+			this.lblPackageNameTitle.Name = "lblPackageNameTitle";
+			this.lblPackageNameTitle.Size = new System.Drawing.Size(131, 20);
+			this.lblPackageNameTitle.TabIndex = 30;
+			this.lblPackageNameTitle.Text = "Service Package:";
+			// 
+			// pnlTicketDisplay
+			// 
+			this.pnlTicketDisplay.BackColor = System.Drawing.SystemColors.ControlDark;
+			this.pnlTicketDisplay.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pnlTicketDisplay.Controls.Add(this.btnAddServicePackage);
+			this.pnlTicketDisplay.Controls.Add(this.lbServicePackages);
+			this.pnlTicketDisplay.Controls.Add(this.lblServicePackagesTitle);
+			this.pnlTicketDisplay.Dock = System.Windows.Forms.DockStyle.Left;
+			this.pnlTicketDisplay.Location = new System.Drawing.Point(0, 0);
+			this.pnlTicketDisplay.Name = "pnlTicketDisplay";
+			this.pnlTicketDisplay.Size = new System.Drawing.Size(151, 450);
+			this.pnlTicketDisplay.TabIndex = 29;
+			// 
+			// btnAddServicePackage
+			// 
+			this.btnAddServicePackage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnAddServicePackage.Location = new System.Drawing.Point(11, 405);
+			this.btnAddServicePackage.Name = "btnAddServicePackage";
+			this.btnAddServicePackage.Size = new System.Drawing.Size(126, 39);
+			this.btnAddServicePackage.TabIndex = 2;
+			this.btnAddServicePackage.Text = "Add Package";
+			this.btnAddServicePackage.UseVisualStyleBackColor = true;
+			// 
+			// lbServicePackages
+			// 
+			this.lbServicePackages.FormattingEnabled = true;
+			this.lbServicePackages.Location = new System.Drawing.Point(3, 31);
+			this.lbServicePackages.Name = "lbServicePackages";
+			this.lbServicePackages.Size = new System.Drawing.Size(143, 368);
+			this.lbServicePackages.TabIndex = 1;
+			// 
+			// lblServicePackagesTitle
+			// 
+			this.lblServicePackagesTitle.AutoSize = true;
+			this.lblServicePackagesTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblServicePackagesTitle.Location = new System.Drawing.Point(11, 8);
+			this.lblServicePackagesTitle.Name = "lblServicePackagesTitle";
+			this.lblServicePackagesTitle.Size = new System.Drawing.Size(129, 20);
+			this.lblServicePackagesTitle.TabIndex = 0;
+			this.lblServicePackagesTitle.Text = "Assigned Tickets";
+			// 
+			// ServiceMetricsForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.lblPerformanceAmount);
+			this.Controls.Add(this.lblperformanceTitle);
+			this.Controls.Add(this.lblSubcriberAmount);
+			this.Controls.Add(this.lblSubcriberAmountTitle);
+			this.Controls.Add(this.txtDescription);
+			this.Controls.Add(this.btnUpdatePackage);
+			this.Controls.Add(this.txtCloseDate);
+			this.Controls.Add(this.lblCloseDateTitle);
+			this.Controls.Add(this.txtReleaseDate);
+			this.Controls.Add(this.lblDescripionTitle);
+			this.Controls.Add(this.lblReleaseDateTitle);
+			this.Controls.Add(this.lblPackageName);
+			this.Controls.Add(this.lblPackageNameTitle);
+			this.Controls.Add(this.pnlTicketDisplay);
+			this.Name = "ServiceMetricsForm";
+			this.Text = "ServiceMetricsForm";
+			this.pnlTicketDisplay.ResumeLayout(false);
+			this.pnlTicketDisplay.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
         }
 
-        #endregion
-    }
+		#endregion
+
+		private System.Windows.Forms.Label lblPerformanceAmount;
+		private System.Windows.Forms.Label lblperformanceTitle;
+		private System.Windows.Forms.Label lblSubcriberAmount;
+		private System.Windows.Forms.Label lblSubcriberAmountTitle;
+		private System.Windows.Forms.TextBox txtDescription;
+		private System.Windows.Forms.Button btnUpdatePackage;
+		private System.Windows.Forms.TextBox txtCloseDate;
+		private System.Windows.Forms.Label lblCloseDateTitle;
+		private System.Windows.Forms.TextBox txtReleaseDate;
+		private System.Windows.Forms.Label lblDescripionTitle;
+		private System.Windows.Forms.Label lblReleaseDateTitle;
+		private System.Windows.Forms.Label lblPackageName;
+		private System.Windows.Forms.Label lblPackageNameTitle;
+		private System.Windows.Forms.Panel pnlTicketDisplay;
+		private System.Windows.Forms.Button btnAddServicePackage;
+		private System.Windows.Forms.ListBox lbServicePackages;
+		private System.Windows.Forms.Label lblServicePackagesTitle;
+	}
 }
