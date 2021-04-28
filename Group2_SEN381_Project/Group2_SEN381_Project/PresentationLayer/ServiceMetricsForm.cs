8ac94ede33711c78d0a171868872da3cd92e6381
Jacques-BGCampus <576464@student.belgiumcampus.ac.za>
2021-04-28 09:20:20 +0200

    Added ServiceMetrics-Form Code

--- Group2_SEN381_Project/Group2_SEN381_Project/PresentationLayer/ServiceMetricsForm.cs
+++ Group2_SEN381_Project/Group2_SEN381_Project/PresentationLayer/ServiceMetricsForm.cs
@@ -8,25 +8,61 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Group2_SEN381_Project.BusinessLogicLayer;
+using Group2_SEN381_Project.DataAccessLayer;
 
 namespace Group2_SEN381_Project.PresentationLayer
 {
-    public partial class ServiceMetricsForm : Form
-    {
-        public ServiceMetricsForm(SatisfactionEmployee satisfactionEmployee)
-        {
-            InitializeComponent();
-        }
+	public partial class ServiceMetricsForm : Form
+	{
+		List<ServicePackage> spList;
 
-        private void btnUpdatePackage_Click(object sender, EventArgs e)
-        {
+		public ServiceMetricsForm(SatisfactionEmployee satisfactionEmployee)
+		{
+			InitializeComponent();
+			spList = ServicePackageHandler.GetAllSP();
+			lbServicePackages.DataSource = spList;
+		}
 
-        }
+		private void btnAddServicePackage_Click(object sender, EventArgs e)
+		{
+			CreateServicePackageForm newForm = new CreateServicePackageForm();
+			newForm.Show();
+		}
 
-        private void btnAddServicePackage_Click(object sender, EventArgs e)
-        {
-            CreateServicePackageForm createServicePackageForm = new CreateServicePackageForm();
-            createServicePackageForm.Show();
-        }
-    }
+		private void btnUpdatePackage_Click(object sender, EventArgs e)
+		{
+			int index = lbServicePackages.SelectedIndex;
+			if (index == -1)
+			{
+				MessageBox.Show("Service Package not found.");
+			}
+			else
+			{
+				ServicePackage selectedSP = spList[index];
+				ServicePackageHandler.UpdateSP(selectedSP.SPID, selectedSP.SPName, txtDescription.Text, txtReleaseDate.Text, txtReleaseDate.Text);
+			}
+		}
+
+		private void lbServicePackages_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			int index = lbServicePackages.SelectedIndex;
+			if (index == -1)
+			{
+				MessageBox.Show("Service Package not found.");
+			}
+			else
+			{
+				ServicePackage selectedSP = spList[index];
+				lblPackageName.Text = selectedSP.SPName;
+				txtReleaseDate.Text = selectedSP.SPReleaseDate;
+				txtCloseDate.Text = selectedSP.SPCloseDate;
+				txtDescription.Text = selectedSP.SPDesc;
+			}
+
+			// TO DO:
+			// Display Service Package subcriber-count
+			// Display Performance metrics
+		}
+
+	}
 }
