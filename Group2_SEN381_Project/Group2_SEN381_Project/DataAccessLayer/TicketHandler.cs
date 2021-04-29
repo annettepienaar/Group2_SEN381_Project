cb92adbfed2d9a82a9b8e10593ce9d7c1cf50875
Annette Pienaar <576370@student.belgiumcampus.ac.za>
2021-04-29 17:08:05 +0200

    Commented everything out on TicketHandler

--- Group2_SEN381_Project/Group2_SEN381_Project/DataAccessLayer/TicketHandler.cs
+++ Group2_SEN381_Project/Group2_SEN381_Project/DataAccessLayer/TicketHandler.cs
@@ -10,97 +10,99 @@ namespace Group2_SEN381_Project.DataAccessLayer
 {
     public static class TicketHandler
     {
-        //adds a ticket to the database
-        public static void Add(Ticket obj)
-        {
-            DataAccess access = new DataAccess();
-            Ticket ticket = AssignTechnicians(obj);
-            access.InsertTicket(ticket.TicketDesc, ticket.TicketLevel, ticket.TicketState, ticket.TicketOpenDate, ticket.TicketCloseDate, ticket.ProblemArea, ticket.ClientID, ticket.TechnitionID, ticket.CallCenterEmpID);
 
-        }
+
+        //adds a ticket to the database
+        //public static void Add(Ticket obj)
+        //{
+        //    DataAccess access = new DataAccess();
+        //    Ticket ticket = AssignTechnicians(obj);                                                                                                                            //Remove TechnicianID
+        //    access.InsertTicket(ticket.TicketDesc, ticket.TicketLevel, ticket.TicketState, ticket.TicketOpenDate, ticket.TicketCloseDate, ticket.ProblemArea, ticket.ClientID, ticket.TechnitionID, ticket.CallCenterEmpID);
+
+        //}
 
         //assigns an open technician to the ticket.
-        public static Ticket AssignTechnicians(Ticket obj)
-        {
-            string problemArea = obj.ProblemArea;
-            int min = int.MaxValue;
-            string techID = "";
-            DataAccess access = new DataAccess();
-            DataTable data = new DataTable();
-            data = access.GetActiveTickets(problemArea);
-            System.Windows.Forms.MessageBox.Show(data.Rows[0].ToString());
+        //public static Ticket AssignTechnicians(Ticket obj)
+        //{
+        //    string problemArea = obj.ProblemArea;
+        //    int min = int.MaxValue;
+        //    string techID = "";
+        //    DataAccess access = new DataAccess();
+        //    DataTable data = new DataTable();
+        //    data = access.GetActiveTickets(problemArea);
+        //    System.Windows.Forms.MessageBox.Show(data.Rows[0].ToString());
 
-            Dictionary<string, int> technicianOpenTickets = new Dictionary<string, int>();
+        //    Dictionary<string, int> technicianOpenTickets = new Dictionary<string, int>();
 
-            foreach (DataRow dr in data.Rows)
-            {
-                System.Windows.Forms.MessageBox.Show($"{dr[0].ToString()} {int.Parse(dr[1].ToString())}");
-                technicianOpenTickets.Add(dr[0].ToString(), int.Parse(dr[1].ToString()));
-            }
-            foreach (KeyValuePair<string,int> item in technicianOpenTickets)
-            {
-                //System.Windows.Forms.MessageBox.Show($"{item.Key} {item.Value}");
-                if (item.Value < min)
-                {
-                    min = item.Value;
-                    techID = item.Key;
-                }
-            }
+        //    foreach (DataRow dr in data.Rows)
+        //    {
+        //        System.Windows.Forms.MessageBox.Show($"{dr[0].ToString()} {int.Parse(dr[1].ToString())}");
+        //        technicianOpenTickets.Add(dr[0].ToString(), int.Parse(dr[1].ToString()));
+        //    }
+        //    foreach (KeyValuePair<string,int> item in technicianOpenTickets)
+        //    {
+        //        //System.Windows.Forms.MessageBox.Show($"{item.Key} {item.Value}");
+        //        if (item.Value < min)
+        //        {
+        //            min = item.Value;
+        //            techID = item.Key;
+        //        }
+        //    }
 
-            obj.TechnitionID = techID;
-            //System.Windows.Forms.MessageBox.Show(obj.TechnitionID);
+        //    obj.TechnitionID = techID;
+        //    //System.Windows.Forms.MessageBox.Show(obj.TechnitionID);
 
-            return obj;
+        //    return obj;
 
-        }
+        //}
         //returns a list of specializations for technician objects
-        public static List<Specialization> GetTechnicianSpecs(string empID)
-        {
-            DataAccess access = new DataAccess();
-            List<Specialization> specs = new List<Specialization>();
-            DataTable data = access.GetTechSpecializations(empID);
+        //public static List<Specialization> GetTechnicianSpecs(string empID)
+        //{
+        //    DataAccess access = new DataAccess();
+        //    List<Specialization> specs = new List<Specialization>();
+        //    DataTable data = access.GetTechSpecializations(empID);
 
-            foreach (DataRow dr in data.Rows)
-            {
-                specs.Add(new Specialization(dr["Spec_ID"].ToString(), dr["Spec_Name"].ToString(), dr["Spec_Description"].ToString()));
-            }
+        //    foreach (DataRow dr in data.Rows)
+        //    {
+        //        specs.Add(new Specialization(dr["Spec_ID"].ToString(), dr["Spec_Name"].ToString(), dr["Spec_Description"].ToString()));
+        //    }
 
-            return specs;
-        }
+        //    return specs;
+        //}
 
         //Returns a specific ticket based on a ticket-ID
-        public static Ticket GetTicket(string ticketID)
-	    {
-            DataAccess dataAccess = new DataAccess();
-            DataTable dataTable = new DataTable();
+     //   public static Ticket GetTicket(string ticketID)
+	    //{
+     //       DataAccess dataAccess = new DataAccess();
+     //       DataTable dataTable = new DataTable();
 
-            dataTable = dataAccess.SearchTicket(ticketID);
-            DataRow row = dataTable.Rows[0];
+     //       dataTable = dataAccess.SearchTicket(ticketID);
+     //       DataRow row = dataTable.Rows[0];
 
             
-            return new Ticket(row[0].ToString(),row[1].ToString(), row[2].ToString(), row[6].ToString(), row[7].ToString(), row[8].ToString(), row[3].ToString(), row[4].ToString(), row[5].ToString(), row[6].ToString());
-        }
+     //       return new Ticket(row[0].ToString(),row[1].ToString(), row[2].ToString(), row[6].ToString(), row[7].ToString(), row[8].ToString(), row[3].ToString(), row[4].ToString(), row[5].ToString(), row[6].ToString());
+     //   }
 
         //Gets list of open tickets for specific Technician
-        public static List<Ticket> GetTechOpenTickets(string empID)
-        {
-            List<Ticket> tickets = new List<Ticket>();
-            DataAccess access = new DataAccess();
-            DataTable data = access.GetTechOpenTicket(empID);
+        //public static List<Ticket> GetTechOpenTickets(string empID)
+        //{
+        //    List<Ticket> tickets = new List<Ticket>();
+        //    DataAccess access = new DataAccess();
+        //    DataTable data = access.GetTechOpenTicket(empID);
 
-            foreach(DataRow dr in data.Rows)
-            {
-                tickets.Add(new Ticket(dr["Ticket_ID"].ToString(), dr["Ticket_Description"].ToString(), dr["Ticket_Level"].ToString(), dr["Ticket_State"].ToString(),dr["Open_Date"].ToString(), dr["Close_Date"].ToString(), dr["Problem_Area"].ToString(), dr["Client_ID"].ToString(), dr["Technician_ID"].ToString(), dr["Call_Center_ID"].ToString()));
-            }
+        //    foreach(DataRow dr in data.Rows)
+        //    {
+        //        tickets.Add(new Ticket(dr["Ticket_ID"].ToString(), dr["Ticket_Description"].ToString(), dr["Ticket_Level"].ToString(), dr["Ticket_State"].ToString(),dr["Open_Date"].ToString(), dr["Close_Date"].ToString(), dr["Problem_Area"].ToString(), dr["Client_ID"].ToString(), dr["Technician_ID"].ToString(), dr["Call_Center_ID"].ToString()));
+        //    }
 
-            return tickets;
-        }
+        //    return tickets;
+        //}
         //Update Ticket
-        public static void UpdateTicket(Ticket obj)
-        {
-            DataAccess access = new DataAccess();
-            access.UpdateTicket(obj.TicketID, obj.TicketDesc, obj.TicketLevel, obj.TicketState, obj.TicketOpenDate, obj.TicketCloseDate, obj.ClientID, obj.TechnitionID, obj.CallCenterEmpID);
-        }
+        //public static void UpdateTicket(Ticket obj)
+        //{
+        //    DataAccess access = new DataAccess();
+        //    access.UpdateTicket(obj.TicketID, obj.TicketDesc, obj.TicketLevel, obj.TicketState, obj.TicketOpenDate, obj.TicketCloseDate, obj.ClientID, obj.TechnitionID, obj.CallCenterEmpID);
+        //}
 
     }
 }
